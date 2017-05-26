6dbce1219a0681b6084a2f70aaf779b50252585c
Sarah Elliott <elliott@blogin3.lcrc.anl.gov>
2017-05-26 14:15:22 -0500

    now can handle multiple reactants and products, TS and VWwells not functional

--- build.py
+++ build.py
@@ -7,45 +7,43 @@ import sys
 sys.path.insert(0, '/home/elliott/Packages/QTC/')
 import iotools as io
 
-class REAC:
-    def __init__(self,smiles,opts):
+class MOL:
+    def __init__(self,opts,typemol = 'reac'):
        
-        self.QTC = opts[3]#if QTC, openbabel, and pybel are not importable, self.QTC = 'False' 
-                          #be sure to have cartesian <SMILE string>.xyz in data/ (ex. C-C-C.xyz)
-      
+        self.convert    = io.get_path('/home/elliott/Packages/TorsScan/test_chem') 
+        self.typemol    = typemol
+
         #OPTIONS##############################
         self.nsamps     = opts[0]    #number of MonteCarlo sampling points
         self.interval   = opts[1]    #Interval for tors scan (should be same geometry as 0 degree)
         self.nsteps     = opts[2]    #Number of points to take on PES
+        self.QTC        = opts[3]    #if QTC, openbabel, and pybel are not importable, self.QTC = 'False' 
+                                     #be sure to have cartesian <SMILE string>.xyz in data/ (ex. C-C-C.xyz)
         ######################################
  
-        self.smiles     = smiles  #Name of molecule
-        self.cart       = os.getcwd() + '/' +  smiles + '.xyz'
-        self.convert    = io.get_path('/home/elliott/Packages/TorsScan/test_chem') #self.convert    = io.get_path('test_chem')
-        self.zmat       = 'reac1.dat'
 
-    def build_cart(self):
+    def build_cart(self,smiles):
         """
         Uses QTC interface by Murat Keceli to Openbabel to generate cartesian coorinate file based 
         on SMILE string
         """
         import obtools as ob
         
-        mol = ob.get_mol(self.smiles)
+        mol = ob.get_mol(smiles)
+
         self.charge = ob.get_charge(mol)
         self.mult   = ob.get_multiplicity(mol)
-        self.stoich = ob.get_formula(mol)
+        stoich = ob.get_formula(mol)
 
-        lines =  ob.get_xyz(mol).split('\n')
+        lines    =  ob.get_xyz(mol).split('\n')
         lines[0] = 'Geometry ' + lines[0] + ' Angstrom'
         del lines[1]
-        io.write_file('\n'.join(lines), self.smiles + '.xyz')
 
-        return
-    def get_stoich(self):
-        return self.stoich
+        io.write_file('\n'.join(lines), smiles + '.xyz')
 
-    def read_cart(self): 
+        return stoich
+
+    def read_cart(self,smiles): 
         """
         Runs Test_Chem by Yuri Georgievski on a file of Cartesian coordinates and collects
         the internal coordinate and torsional angle information that it outputs
@@ -58,15 +56,16 @@ class REAC:
         angles  = []
 
         if self.QTC.lower() == 'true':
-            self.build_cart()
-        else: #we assume an xyz is given and hope default charge/mult work
+            stoich = self.build_cart(smiles)
+        else:     #If we can't use QTC to build_cart we assume an xyz is given and hope default charge/mult work
             self.charge = 0
             self.mult   = 1
-            self.stoich = self.smiles
+            stoich = smiles
 
-        #open cartesian input file
+        #Peform Test_Chem
         tempfile = 'temp'
-        os.system(self.convert + ' ' + self.cart + ' > ' + tempfile)
+        cart       = os.getcwd() + '/' +  smiles + '.xyz'
+        os.system(self.convert + ' ' + cart + ' > ' + tempfile)
 
         if os.stat(tempfile).st_size == 0:
             print('failed')
@@ -74,46 +73,42 @@ class REAC:
             print('Please check that test_chem is in location: ' +  self.convert)
             return atoms, measure, angles
         
+        #Get relevant data from Test_Chem output file
         props,lines = io.read_file(tempfile).split('Z-Matrix:\n')
+        io.rm(tempfile)
+
         lines = lines.split('\n')
         for i,line in enumerate(lines):
-            #collect the connectivity once Z-Matrix is begun
             if line == '':
                 break
-            atoms.extend([line.rstrip('\n').split(',')])
+            atoms.extend([line.rstrip('\n').split(',')])                        #connectivity
+
         for j in range(i+1,len(lines)):
-            #collect the bond lengths, bond angles, and dihedral angles
             if 'Rot' in lines[j]:
                 break
-            measure.extend(lines[j].replace('=',' ').rstrip('\n').split())
-        #record angles to scan
-        if lines[j].split(':')[1].rstrip('\n').strip() != '':
-            angles  = lines[j].split(':')[1].strip().split(',')
+            measure.extend(lines[j].replace('=',' ').rstrip('\n').split()) #bond lengths, bond angles, and dihedral angles
 
-        #Linearity
-        if re.search("molecule is (\w+)", props).groups()[0] == 'nonlinear':
+        if lines[j].split(':')[1].rstrip('\n').strip() != '':
+            angles  = lines[j].split(':')[1].strip().split(',')                #angles to scan
+
+        if re.search("molecule is (\w+)", props).groups()[0] == 'nonlinear':        #Linearity
             self.ilin = ' 0'
         else:
             self.ilin = ' 1'
            
-        #Symmetry factor
-        self.symnum = re.search("symmetry number = (\w+)", props).groups()[0]
+        self.symnum = re.search("symmetry number = (\w+)", props).groups()[0] #Symmetry factor
             
-        ##Beta-scission, may use eventually
-        #self.beta = re.search("Beta-scission bonds: (\w+)", line).groups()[0]
+        measure = np.array(measure)                     
+        measure = measure.reshape( len(measure)/2, 2)      #Puts measurements into two columns
 
-        measure = np.array(measure)
-        measure = measure.reshape( len(measure)/2, 2)
-
-        return atoms, measure, angles
+        return stoich, atoms, measure, angles
     
-    def update_interns(self):
+    def update_interns(self,smiles):
         """
         Converts internal coordinate information from Test_Chem to the form for
         a Z-Matrix required to run EStokTP
         """
-        #Converts ZMAT into the format required for EStokTP
-        atoms, measure, angles = self.read_cart()
+        stoich, atoms, measure, angles = self.read_cart(smiles)             #Converts ZMAT to EStokTP format
 
         if atoms == 0:
             return  
@@ -127,7 +122,7 @@ class REAC:
                     if len(atom) > 5:
                         atoms[index][5] = atoms[int(atoms[index][5])-1][0]
 
-        return atoms, measure, angles
+        return stoich, atoms, measure, angles
 
     def find_period(self,zmat,hin):
         """
@@ -150,13 +145,13 @@ class REAC:
                     sym2 += 1
         return max(sym1,sym2)
 
-    def build(self):
+    def build(self,smiles,n):
         """ 
-        Builds reac1.dat for EStokTP withh user defined nosmps (Monte Carlo sampling points
+        Builds reacn.dat or prodn.dat for EStokTP withh user defined nosmps (Monte Carlo sampling points
         for geometry search) and nhindsteps (number of points on the PES) 
         """
 
-        atoms, measure, angles = self.update_interns()
+        stoich, atoms, measure, angles = self.update_interns(smiles)
         #Stochastic Geometry Search############
         zmatstring  = 'nosmp dthresh ethresh\n'     
         zmatstring += self.nsamps + '  1.0  0.00001\n'
@@ -203,23 +198,22 @@ class REAC:
         zmatstring += '\n\nSymmetryFactor\n' + self.symnum + '\n'
         zmatstring += '\nnelec\n1\n 0.  1.\n\nend\n'
 
-        io.write_file(zmatstring, self.zmat)
+        zmat       = self.typemol +str(n) + '.dat'
+        io.write_file(zmatstring, zmat)
 
-        return 
+        return stoich
 
 class THEORY:
     def __init__(self,meths):
-        start = 0
         self.meths = meths
  
     def build(self):
         """
         Builds theory.dat 
         """
-
         meths    = self.meths
-        theory  = ''
 
+        theory  = ''
         for meth in meths:
             theory += meth[0] + ' ' + meth[1] + '\n '
             theory += meth[2] + ' opt=internal\n'
@@ -227,7 +221,6 @@ class THEORY:
             if meth[0] == 'level1':
                 theory += ' freq'
             theory += '\n\n'
-
         theory += 'End'
 
         io.write_file(theory, 'theory.dat')
@@ -235,21 +228,46 @@ class THEORY:
         return 
 
 class ESTOKTP:
-    def __init__(self,stoich,jobs):
+    def __init__(self,stoich,jobs,opts,nreacs,nprods):
+
         self.stoich  = stoich
         self.jobs    = jobs
+        self.nreacs  = nreacs
+        self.nprods  = nprods
+        self.coresh  = opts[0]
+        self.coresl  = opts[1]
                  
-    def build(self):
+    def build(self,reactype,nTS):
        """
        Builds esktoktp.dat
        """
 
        eststring  = ' Stoichiometry\t' + self.stoich.upper()
+     
+       PossibleRxns = {'addition','abstraction','isomerization','betascission','well',''}
+       if reactype.lower() not in PossibleRxns:
+           print('ReactionType ' + reactype +' is unrecognized, please use: Addition, Abstraction, Isomerization, or Betascission')
+       elif reactype != '' and reactype.lower() != 'well':
+           eststring  += '\n ReactionType\t' + reactype
+           if nTS != 0:
+               eststring += ' ' + str(nTS) + 'TS'
+
        eststring +='\n Debug  2'
+
        for job in self.jobs:
-           eststring += '\n ' +job
+           for n in range(self.nreacs):
+               if job == 'Opt_1':
+                   eststring += '\n ' + 'Opt_Reac' + str(n+1) + '_1'
+               else:
+                   eststring += '\n ' +job + '_Reac' + str(n+1)
+           for n in range(self.nprods):
+               if job == 'Opt_1':
+                   eststring += '\n ' + 'Opt_Prod' + str(n+1) + '_1'
+               else:
+                   eststring += '\n ' +job + '_Prod' + str(n+1)
+
        eststring += '\nEnd'
-       eststring += '\n 10,6\n numprocll,numprochl\n'
+       eststring += '\n ' + self.coresh + ',' + self.coresl + '\n numprocll,numprochl\n'
        eststring += ' 200MW  300MW\n gmemll gmemhl\n'
        io.write_file(eststring, 'estoktp.dat')
 
--- torsional_scan.py
+++ torsional_scan.py
@@ -3,23 +3,29 @@
 import os
 import sys
 import build
-
+import numpy as np
 sys.path.insert(0, './bin')
 sys.path.insert(0, '/home/elliott/Packages/QTC/')
 import iotools as io
 
-class Tscan:
+class ES:
     def __init__(self):
-        ####DEFAULT INPUTS###############################
-        self.mol  = 'C-C-C' #SMILE string
-        self.QTC  = 'True'  #QTC, Openbabel, and Pybel installed
-        self.node = 'debug' #Default node to run on in is debug (won't run)
-        self.nsamps   = '5' #Number of MC sampling points
-        self.interval = 360 #Interval to scan
-        self.nsteps   = '4' #Number of steps on PES
-        ############################################
+    
+        ####DEFAULT INPUTS#########################
+        self.reacs    = 'CCC'   #list of SMILE strings of reactants
+        self.prods    = ''      #list of SMILE strings of products
+        self.reactype = ''      #type of reaction (default well)
+        self.nTS      = 0       #Number of transition states (default 0)
+        self.QTC      = 'True'  #QTC, Openbabel, and Pybel installed
+        self.node     = 'debug' #Default node to run on in is debug (won't run)
+        self.coresh   = '10'    #Default high number of cores is 10
+        self.coresl   = '6'     #Default low number of cores is 10
+        self.nsamps   = '5'     #Number of MC sampling points
+        self.interval = 360     #Interval to scan
+        self.nsteps   = '4'     #Number of steps on PES
+        ###########################################
 
-        self.get_options()
+        self.get_options()      #Options from input file
        
     def get_options(self):
         """
@@ -27,16 +33,22 @@ class Tscan:
         """
         options = io.read_file('input.dat')
 
-        self.set_theory(options)
+        self.get_theory_params(options)
 
         options      = options.split('\n')
 
-        self.node    = self.set_param('node'    , options)
-        self.QTC     = self.set_param('Use QTC' , options)
-        self.mol     = self.set_param('Molecule', options)
-        self.nsamps  = self.set_param('sampling', options)
-        self.interval= self.set_param('interval', options)
-        self.nsteps  = self.set_param('steps'   , options)
+        self.reactype= self.get_param(self.reactype, 'Reaction type', options)
+        self.nTS     = self.get_param(self.reactype, 'transition'   , options)
+        self.reacs   = self.get_param(self.reacs   , 'Reactant'     , options)
+        self.prods   = self.get_param(self.prods   , 'Product'      , options)
+        self.node    = self.get_param(self.node    , 'node'         , options)
+        self.coresh  = self.get_param(self.coresh  , 'cores high'   , options)
+        self.coresl  = self.get_param(self.coresl  , 'cores low'    , options)
+        self.QTC     = self.get_param(self.QTC     , 'Use QTC'      , options)
+        self.nsamps  = self.get_param(self.nsamps  , 'sampling'     , options)
+        self.interval= self.get_param(self.interval, 'interval'     , options)
+        self.nsteps  = self.get_param(self.nsteps  , 'steps'        , options)
+
         return
        
     def build_subdirs(self):
@@ -55,20 +67,33 @@ class Tscan:
     def build_files(self):
 
         """
-        Runs the build functions for reac1.dat, theory.dat, and estoktp.dat
-        Requirements: QTC, Openbabel, Pybel
+        Runs the build functions for reacn.dat, prodn.dat, theory.dat, and estoktp.dat
+        Requirements: QTC, Openbabel, Pybel (OR prepared cartesian coordinate files) and Test_Chem
         """
         if self.QTC.lower() == 'skip': 
-            print('task skipped')
+            print('Skipping build of data/*.dat')
+            self.stoich = self.mol
             return
 
         os.chdir('./data')
-
-        print('Task: Building reac1.dat...')
+        
         opts = (self.nsamps, self.interval,self.nsteps,self.QTC)
-        reac = build.REAC(self.mol,opts)
-        reac.build()
-        print('completed')
+        stoich = []
+        Reac = build.MOL(opts,'reac')
+        Prod = build.MOL(opts,'prod')
+        reacs = self.reacs.split(',')
+        prods = self.prods.split(',')
+        for i, reac in enumerate(reacs):
+            print('Task: Building reac' + str(i+1) + '.dat...')
+            stoich.append(Reac.build(reac.strip(),i+1))
+            print('completed')
+        for j, prod in enumerate(prods):
+            print('Task: Building prod' + str(j+1) + '.dat...')
+            stoich.append(Prod.build(prod.strip(),j+1))
+            print('completed')
+
+        self.stoich = stoich[0]
+        self.mol    =   reac[0]
 
         print('Task: Building theory.dat...')
         theory = build.THEORY(self.meths)
@@ -76,8 +101,9 @@ class Tscan:
         print('completed')
 
         print('Task: Building estoktp.dat...')
-        estoktp = build.ESTOKTP(reac.get_stoich(),self.jobs)
-        estoktp.build()
+        opts    = (self.coresh,self.coresl)
+        estoktp = build.ESTOKTP(self.stoich,self.jobs,opts,i+1,j+1)
+        estoktp.build(self.reactype,self.nTS)
         print('completed')
 
         os.chdir('..')
@@ -89,6 +115,7 @@ class Tscan:
         Runs EStokTP on a given blues node (default b431)
         use 0 in input file to run on login
         use d or debug to just make input files and not run EStokTP
+        Requirements: PACC member on blues
         """
         print('Task: Submitting EStokTP job...')
         if self.node == '0':
@@ -100,41 +127,112 @@ class Tscan:
             os.system('~/bin/run_estoktp.com ' + self.node)
         print('completed')
         return
+    
+    def get_val(self,opt,result,endl = 'True'):
+        """
+        Gets values we care about from a line of input file
+        """
+        if endl == 'True':
+            return opt.split(':')[result].strip('\n').strip()
+        else:
+            return opt.split(':')[result].strip()
 
-    def build_mess(self):
+    def key_check(self,line,keyword):
+        """                                                   
+        Checks if a line of the input file has a keyword on it
+        """                                                   
+        if keyword in line:                                   
+            return True                                       
+        else:                                                 
+            return False                                     
+       
+    def get_param(self,param,keyword,inputlines):
+         """
+         Sets parameter based on a keyword in inputfile
+         """
+         for line in inputlines:
+             if self.key_check(line,keyword):
+                 return  self.get_val(line,1)
+         return param
+      
+    def get_theory_params(self,inputlines):
+        """
+        Sets theory parameters
+        """
+        comps = {'Opt':'level0','Opt_WellP':'level0','Opt_WellR':'level0','Grid_Opt_TS':'level0',
+                           'Opt_TS_0':'level0_ts','TauO_TS':'level0_ts','Opt_1':'level1',
+                           'Opts_TS_1':'level1_ts','1dTau':'hind_rotor','MdTau':'hind_rotor',
+                           '1dTau_TS':'hind_rotor_ts','MdTau_TS':'hind_rotor_ts','Symm':'symmetry',
+                           'Symm_TS':'symmetry_ts','HL':'hlevel',
+                           'HL_TS':'hlevel_ts','IRC':'irc'}
+                   #'Opt_WellP_1':'level1','Opt_WellR_1':'level1','1dTau_WellR':'hind_rotor', 
+                   #'1dTau_WellP':'hind_rotor','MdTau_WellR':'hind_rotor','MdTau_WellP':'hind_rotor',
+                   #'Symm_WellR':'symmetry','Symm_WellP':'symmetry','HL_WellP':'hlevel','HL_WellR':'hlevel',
+        inputlines = inputlines.replace(' ','')
+        lines      = inputlines.split('------------------------------')[2].strip('-').split('\n')
+        del lines[0]
+        self.jobs  = []
+        self.meths = []
+        templist   = []
+        for line in lines:
+            line = line.strip().split(':')
+            if self.key_check(comps,line[0]) and line[1] != '':
+                if self.key_check(templist,comps[line[0]]) ==  False:
+                    self.meths.append([comps[line[0]],line[1],line[2]])
+                    templist.append(comps[line[0]])
+                self.jobs.append(line[0])
+            elif line[0] != '':
+                if line[1] != '':
+                    print (line[0] + ' is not a recognized module')
+        return
+
+
+class MESS:
+    def __init__(self,mol,stoich):
+        self.mol      = mol
+        self.stoich   = stoich
+
+    def build(self):
         """
         Compiles together the mess data extracted from EStokTP computations
         to form an mess input file
         """
-        print('Task: Extracting data for MESS...')
         if not os.path.exists('me_files'):
             print('failed -- me_files not found, check estoktp.log')
             return
 
-        #Copy first section of QTC pf file
-        pfqtc = self.extract_mess(self.mol + '.pf').split('\n')
+        print('Task: Extracting data for MESS...')
+        os.chdir('me_files')
+        ge = self.extract_mess('reac1_1dge.me')                      #Copy EStokTP geometry
+        ge = " Species " + self.stoich +  ge.split("Species")[1]
+        hr = self.extract_mess('reac1_hr.me')                         #Copy EStokTP hr data
+        fr = self.extract_mess('reac1_fr.me')                 #Copy EStokTP projfrequencies
+        fr = fr.split('End')[0] + 'End  '
+        os.chdir('..')                            # (projected out scanned torsional modes)
+        print('completed')
+
+        print('Task: Building MESS input file...')
+        pfqtc = self.extract_mess(self.mol + '.pf').split('\n') #Copy 1st section of QTC pf file
         pf   = ''
         for line in pfqtc:
             #if line == 'Frequencies': #use QTC geometry
             #   break
-            if "Species" in line: #Use EStokTP geometry
+            if "Species" in line:                                     #Use EStokTP geometry
                break
             pf += line + '\n'
-        
-        os.chdir('me_files')
-        #EStokTP geometry
-        ge = self.extract_mess('reac1_1dge.me')
-        ge = " Species" +  ge.split("Species")[1]
-        #Use EStokTP hr data
-        hr = self.extract_mess('reac1_hr.me')
-        #Use EStokTP frequencies (projected out scanned torsional modes)
-        fr = self.extract_mess('reac1_fr.me')
-
         pf += ge + hr + fr
-        os.chdir('..')
         io.write_file(pf,'new_' + self.mol + '.pf')
+        print('completed')
 
         return
+
+    def run(self):
+        """
+        Runs mess
+        """
+        print('Task: Running mess')
+        os.system('/home/ygeorgi/build/crossrate/partition_function ' + 'new_' + self.mol + '.pf')
+        print('completed')
 
     def extract_mess(self,filename):
         """
@@ -145,58 +243,13 @@ class Tscan:
             print('failed -- ' + filename + ' is empty, check estoktp.log')
         return lines
 
-    def get_val(self,opt,result,endl = 'True'):
-        """
-        Gets values we care about from a line of input file
-        """
-        if endl == 'True':
-            return opt.split(':')[result].strip('\n').strip()
-        else:
-            return opt.split(':')[result].strip()
-    def key_check(self,line,keyword):
-        """                                                   
-        Checks if a line of the input file has a keyword on it
-        """                                                   
-        if keyword in line:                                   
-            return True                                       
-        else:                                                 
-            return False                                     
-       
-    def set_param(self,keyword,inputlines):
-         """
-         Sets parameter based on a keyword in inputfile
-         """
-         for line in inputlines:
-             if self.key_check(line,keyword):
-                 return  self.get_val(line,1)
-         return
-      
-    def set_theory(self,inputlines):
-        """
-        Sets theory parameters
-        """
-        comps = {'Opt_Reac1':'level0','Opt_Reac1_1':'level1','1dTau_Reac1':'hind_rotor',
-                           'HL_Reac1':'hind_rotor','Symm_Reac1':'symmetry','kTP':'hlevel'}
-
-        inputlines = inputlines.replace(' ','')
-        lines      = inputlines.split('--------------------------')[2].strip('-').split('\n')
-        del lines[0]
-        self.jobs  = []
-        self.meths = []
-
-        for line in lines:
-            line = line.strip().split(':') 
-            if self.key_check(comps,line[0]):
-                if self.key_check(self.jobs,comps[line[0]]) and line[1] != '':
-                    self.jobs.append(line[0])
-                    self.meths.append([comps[line[0]],line[1],line[2]])
-        return
-
  
 if __name__ == "__main__":
 
-    run = Tscan()
-    run.build_subdirs()
-    run.build_files()
-    run.execute()
-    run.extract()
+    es = ES()
+    es.build_subdirs()
+    es.build_files()
+    es.execute()
+    mess = MESS(es.mol,es.stoich)
+    mess.build()
+    mess.run()
