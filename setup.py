70dd6cc52eaa623c1caf55b74e0feccb8f754460
avcopan <avcopan@gmail.com>
2018-11-20 13:34:36 -0600

    Bumps the version number

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 
 setup(name="rc_driver",
+      version="0.1.1",
       scripts=["rc_driver.py"],
       package_dir={'me_parser': 'external/me_parser/me_parser'},
       packages=['rcd', 'me_parser'])
